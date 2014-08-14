ae265ae7b055face387b883210036dff94d4d029
srinivas <srinivas@10.211.55.3>
2014-08-14 15:00:30 +0100

    Removed few images

--- Salesforce/src/main/java/com/qtpselenium/app/salesforce/login/LoginTest.java
+++ Salesforce/src/main/java/com/qtpselenium/app/salesforce/login/LoginTest.java
@@ -34,14 +34,14 @@ public class LoginTest{
 		selenium.log("Typing in "+object);
 		System.out.println("Entering in "+ object +" value "+ text);
 		selenium.type(text, object);
-		selenium.capturescreenshot("Notes");
+		selenium.capturescreenshot("Enter");
 
     }
 	@And("^I click on \"([^\"]*)\"$")
 	public void I_click_on(String object) throws IOException{
 		selenium.log("Clicking on "+object);
 		selenium.click(object);
-		selenium.capturescreenshot("Login");
+		selenium.capturescreenshot("Click");
 	}
 	
 	@Then("^Login should be \"([^\"]*)\"$")
